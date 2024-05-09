0a2a7de6201d82cf35819246d9761b101cd38b79
Rayane Ait Ali Yahia <rayane.ait.ali.yahia.pro@gmail.com>
2024-05-09 16:29:14 +0200

    update main file

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World it's the Flask CI/CD demo!</p>"
+
+if __name__=="__main__":
+    app.run(host='127.0.0.1', port=8080)
