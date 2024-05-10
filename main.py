4b1cd5c5fea2d802b1f11f19e5918272816c83c2
AIT ALI YAHIA Rayane <aitaliyahia.rayane.pro@gmail.com>
2024-05-10 22:33:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World it's the Flask CI/CD demo!</p>"
+@app.route('/')
+def hello_geek():
+    return '<h1>Hello from Flask & Docker</h2>'
 
-if __name__=="__main__":
-    app.run(host='127.0.0.1', port=8080)
+
+if __name__ == "__main__":
+    app.run(debug=True)
